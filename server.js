92e36ce5fff487bb70b2229f7efb55457cb1382c
Sheraz <sheraz.uos325@gmail.com>
2025-08-24 17:33:36 +0500

    dsl

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ io.on("connection", (socket) => {
     });
 });
 const PORT=process.env.PORT || 3000;
-server.listen(3000,"0.0.0.0", () => {
+server.listen(3000, () => {
     console.log("Server running at : http://0.0.0.0:3000");
 });
